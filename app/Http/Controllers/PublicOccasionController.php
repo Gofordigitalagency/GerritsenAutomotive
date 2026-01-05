f62df109117fbc0276210b1642195e820a0a101c
Bandozz <Bandozie@gmail.com>
2026-01-05 22:02:41 +0100

    header-background & verkocht systeem aangepast

--- app/Http/Controllers/PublicOccasionController.php
+++ app/Http/Controllers/PublicOccasionController.php
@@ -7,17 +7,25 @@ use Illuminate\Http\Request;
 
 class PublicOccasionController extends Controller
 {
-    public function home()
-    {
-        $nieuw = Occasion::latest()->get();
-        return view('home', compact('nieuw'));
-    }
+public function home()
+{
+    $nieuw = Occasion::query()
+        ->orderByRaw("CASE WHEN model LIKE '%(VERKOCHT)%' THEN 1 ELSE 0 END ASC")
+        ->latest()
+        ->get();
 
-    public function index(Request $request)
-    {
-        $occasions = Occasion::latest()->get();
-        return view('occasions.index', compact('occasions'));
-    }
+    return view('home', compact('nieuw'));
+}
+
+public function index(Request $request)
+{
+    $occasions = Occasion::query()
+        ->orderByRaw("CASE WHEN model LIKE '%(VERKOCHT)%' THEN 1 ELSE 0 END ASC")
+        ->latest()
+        ->get();
+
+    return view('occasions.index', compact('occasions'));
+}
 
     public function show($slug)
     {
