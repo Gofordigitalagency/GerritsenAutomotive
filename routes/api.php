b9e11b97ebab0931aa19c87e546b400fcc4b193c
yaelscholten <101642932+yaelscholten@users.noreply.github.com>
2026-06-29 14:02:58 +0200

    Mobilox endpoint: vriendelijke GET-respons i.p.v. 405-foutpagina

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // een XML-POST (add/change/delete). Beveiligd via ?key=<MOBILOX_TOKEN>.
 Route::post('/mobilox/incremental', [MobiloxController::class, 'incremental']);
 
+// Vriendelijke melding bij een GET (bv. URL in de browser of validatie),
+// zodat er geen 405-foutpagina verschijnt.
+Route::get('/mobilox/incremental', function () {
+    return response('Mobilox EWI-endpoint actief. Verstuur voertuigmutaties via een POST (XML).', 200)
+        ->header('Content-Type', 'text/plain; charset=UTF-8');
+});
+
